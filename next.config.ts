2002789814451e713265d40cc65723bedcc47eda
Flatbird <official.flatbird@gmail.com>
2025-11-14 20:38:11 -0800

    make work

--- next.config.ts
+++ next.config.ts
@@ -11,9 +11,5 @@ const nextConfig: NextConfig = {
   },
 };
 
-if (process.env.NODE_ENV === 'development') {
-  const { setupDevPlatform } = require('@cloudflare/next-on-pages/next-dev');
-  setupDevPlatform();
-}
 
 export default nextConfig;
